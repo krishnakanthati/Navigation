fe1d09fa250ae0088ae3f06a8c474e26323c58cb
Krishna Kant Hati <krishnakant08@users.noreply.github.com>
2020-08-08 22:38:06 +0530

    Updates

---
+++ precache-manifest.97903e207c8334c7f0894a0deec78880.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2c02e5681b2bc98c49431283e54534",
+    "url": "/Navigation/index.html"
+  },
+  {
+    "revision": "cd18fecfba495fe249df",
+    "url": "/Navigation/static/css/main.9a23282e.chunk.css"
+  },
+  {
+    "revision": "0b1d53af654a8f19bc17",
+    "url": "/Navigation/static/js/2.5f8626f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Navigation/static/js/2.5f8626f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd18fecfba495fe249df",
+    "url": "/Navigation/static/js/main.c48d4fe4.chunk.js"
+  },
+  {
+    "revision": "f17d957f42d1f04c4578",
+    "url": "/Navigation/static/js/runtime-main.7b683895.js"
+  },
+  {
+    "revision": "48ae6434de48325549277b49f7ebf731",
+    "url": "/Navigation/static/media/arrow.48ae6434.svg"
+  },
+  {
+    "revision": "e9fd40a70989ed93a960628beaf25b7d",
+    "url": "/Navigation/static/media/bell.e9fd40a7.svg"
+  },
+  {
+    "revision": "681afa2ed6c043ea96751e7b755cc561",
+    "url": "/Navigation/static/media/bolt.681afa2e.svg"
+  },
+  {
+    "revision": "615e14d18e96dc6cb0e028991a60ef80",
+    "url": "/Navigation/static/media/caret.615e14d1.svg"
+  },
+  {
+    "revision": "efbb2bd6e6e3066e53de373d26c1e039",
+    "url": "/Navigation/static/media/chevron.efbb2bd6.svg"
+  },
+  {
+    "revision": "993f0cc0e8de3fe0490e28fad0ee23a3",
+    "url": "/Navigation/static/media/cog.993f0cc0.svg"
+  },
+  {
+    "revision": "966e5d311721e82499d16b64e4f26b63",
+    "url": "/Navigation/static/media/home.966e5d31.svg"
+  },
+  {
+    "revision": "9fdc8d67348585808e061c4755bb68f9",
+    "url": "/Navigation/static/media/messenger.9fdc8d67.svg"
+  },
+  {
+    "revision": "9311af2e3926a973133dc6ebe8e8a9aa",
+    "url": "/Navigation/static/media/plus.9311af2e.svg"
+  },
+  {
+    "revision": "33315527eafd161649470a27276a7a37",
+    "url": "/Navigation/static/media/search.33315527.svg"
+  }
+]);
