c87dbaad13ccdc032393184cdce158690ddc9e9a
Krishna Kant Hati <krishnakant08@users.noreply.github.com>
2020-08-08 22:23:31 +0530

    navigation

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+import { ReactComponent as Home } from './icons/home.svg';
+import { ReactComponent as Search } from './icons/search.svg';
+
+function App() {
+  return (
+    <Navbar>
+      <NavItem icon={<Home />} />
+      <NavItem icon={<Search />} />
+      <NavItem icon={<PlusIcon/>} />
+      <NavItem icon={<BellIcon/>} />
+      <NavItem icon={<MessengerIcon/>} />
+      <NavItem icon={<CaretIcon/>} >
+        <DropdownMenu />
+      </NavItem>
+    </Navbar>
+  );
+}
+
+function DropdownMenu() {
+
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
+  }, [])
+
+  function calcHeight (el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+        <span className="icon-button">{props.leftIcon}</span>
+          {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === 'main'}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-primary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="settings"
+          >
+            Settings
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'settings'}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-secondary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon/>}>
+            <h2>Skills</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon="&#128558;">HTML</DropdownItem>
+          <DropdownItem leftIcon="&#128515;">CSS</DropdownItem>
+          <DropdownItem leftIcon="&#128521;">JavaScript</DropdownItem>
+          <DropdownItem leftIcon="&#128520;">React</DropdownItem>
+          <DropdownItem leftIcon="&#128527;">NodeJs</DropdownItem>
+          <DropdownItem leftIcon="&#128565;">Heroku</DropdownItem>
+          <DropdownItem leftIcon="&#129322;">Express</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  );
+}
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className='nav-item'>
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+      {open && props.children}
+    </li>
+  );
+}
+
+export default App;
